f8f57e4164db4757d07ca43cdfb50b0c7fcbe618
ssudipta1989 <phoenix567sid@gmail.com>
2020-05-03 00:30:25 +0530

    Added Test case for ExpenseListFilter component

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -17,7 +17,7 @@ export class ExpenseListFilters extends React.Component {
   onTextChange = (e) => {
     this.props.setTextFilter(e.target.value);
   };
-  onSortChange =(e) => {
+  onSortChange = (e) => {
     if (e.target.value === 'date') {
       this.props.sortByDate();
     } else if (e.target.value === 'amount') {
@@ -58,12 +58,13 @@ const mapStateToProps = (state) => ({
   filters: state.filters
 });
 
-const mapDispatchToProps = () => ({
+const mapDispatchToProps = (dispatch) => ({
   setTextFilter: (text) => dispatch(setTextFilter(text)),
   sortByDate: () => dispatch(sortByDate()),
   sortByAmount: () => dispatch(sortByAmount()),
   setStartDate: (startDate) => dispatch(setStartDate(startDate)),
-  setEndDate : (endDate) => dispatch(setEndDate(endDate))
+  setEndDate: (endDate) => dispatch(setEndDate(endDate))
 });
 
-export default connect(mapStateToProps,mapDispatchToProps)(ExpenseListFilters);
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
+
